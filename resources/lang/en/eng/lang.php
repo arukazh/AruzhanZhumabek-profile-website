a99a9e8d8806d8efbf0941f98f0cbfd8f086871e
arukazh <190103132@stu.sdu.edu.kz>
2021-04-11 21:51:50 +0600

    lang.php file created

---
+++ resources/lang/en/eng/lang.php
@@ -0,0 +1,5 @@
+<?php
+return[ 'welcome' => 'Welcome',
+'title' => 'I am a web developer',
+'body' => "I can help you with web sites"];                                                                            
+?>
